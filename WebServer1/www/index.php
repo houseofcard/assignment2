4f3c00f886bccb8b874e13fd80543e37c3988fbf
houseofcard <53417201+houseofcard@users.noreply.github.com>
2019-09-29 12:35:26 +1300

    Index file for Webserver1

---
+++ WebServer1/www/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
+<html>
+<head>
+<title>Stained Glass Windows</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<h1>Stained Glass Windows</h1>
+
+<p>We offer the following stained glass windows:</p>
+
+<table border="1">
+<tr><th>Window Code</th><th>Window Name</th></tr>
+
+
+</table>
+
+<h2>Select A Stained Glass Window</h2>
+
+<form action="#" method="post">
+<select name="windows">
+
+</select>
+<input type="submit" name="submit" value="Create Link"/>
+</form>
+        
+
+<p>You have selected: </p>
+
+<a href='astrological.php'>Astrological</a>
+    
+    
+<a href='coloured_tree.php'>Coloured Tree</a>
+    
+    
+<a href='gothic.php'>Gothic</a>
+    
+    
+<a href='http://ec2-100-24-105-199.compute-1.amazonaws.com/red_plant.php'>Red Plant</a>
+    
+    
+<a href='http://ec2-100-24-105-199.compute-1.amazonaws.com/religious.php'>Religious</a>    
+  
+<p>Click the link above</p>
+</body>
+</html>
+
